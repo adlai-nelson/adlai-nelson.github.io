fcbf36f31f991760d2dd84e017c7796c0eb46401
adlai-nelson <adlai.nelson123@gmail.com>
2024-11-21 10:50:00 -0500

    added animation to projects

--- script.js
+++ script.js
@@ -48,19 +48,15 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active1");
-
-    /* Toggle between hiding and showing the active panel */
+    this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
-} 
+}
 
 
 // Get the button that opens the modal
@@ -98,3 +94,4 @@ window.onclick = function(event) {
      }
     }
 }
+
