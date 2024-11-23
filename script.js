6909aeb2ad47984f9cc51f8a4b3e5544753f5e67
adlai-nelson <adlai.nelson123@gmail.com>
2024-11-23 18:28:44 -0500

    changed to modals

--- script.js
+++ script.js
@@ -59,8 +59,8 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-// Get the button that opens the modal
-var btn = document.querySelectorAll("button.modal-button");
+// Get the images that open the modal
+var imgs = document.querySelectorAll("img.modal-image");
 
 // All page modals
 var modals = document.querySelectorAll('.modal');
@@ -68,30 +68,32 @@ var modals = document.querySelectorAll('.modal');
 // Get the <span> element that closes the modal
 var spans = document.getElementsByClassName("close");
 
-// When the user clicks the button, open the modal
-for (var i = 0; i < btn.length; i++) {
- btn[i].onclick = function(e) {
-    e.preventDefault();
-    modal = document.querySelector(e.target.getAttribute("href"));
-    modal.style.display = "block";
- }
+// When the user clicks an image, open the modal
+for (var i = 0; i < imgs.length; i++) {
+    imgs[i].onclick = function(e) {
+        e.preventDefault();
+        var modal = document.querySelector(e.target.getAttribute("href"));
+        modal.style.display = "block";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
 for (var i = 0; i < spans.length; i++) {
- spans[i].onclick = function() {
-    for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    spans[i].onclick = function() {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+        }
     }
- }
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
-     for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-     }
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+        }
     }
 }
+
+
 
